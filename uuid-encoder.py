88a8f766bd2b44dadabb83805aaea985dcb3c857
icaoberg <icaoberg@andrew.cmu.edu>
2023-04-10 12:46:20 -0400

    Refactoring: Removed unnecessary imports and functions. Added a function to generate UUID from directory path.

--- uuid-encoder.py
+++ uuid-encoder.py
@@ -1,27 +1,19 @@
 import argparse
-
-import dask.dataframe as dd
-import numpy as np
-import pandas as pd
-import tabulate
-from dask import delayed
-from joblib import Parallel, delayed
-from pandarallel import pandarallel
-from PIL import Image
-from tqdm import tqdm
+import uuid
 
 
-def pprint(msg):
+def __pprint(msg):
     row = len(msg)
     h = "".join(["+"] + ["-" * row] + ["+"])
     result = h + "\n" "|" + msg + "|" "\n" + h
     print(result)
 
 
-def __update_dataframe(dataset, temp, key):
-    for index, datum in temp.iterrows():
-        dataset.loc[index, key] = temp.loc[index, key]
-    return dataset
+def __generate_dataset_uuid(directory):
+    if directory[-1] == "/":
+        directory = directory[:-1]
+
+    return str(uuid.uuid5(uuid.NAMESPACE_DNS, directory))
 
 
 ###############################################################################################################
@@ -31,10 +23,4 @@ parser.add_argument("-d", "--directory", dest="directory", help="Directory")
 args = parser.parse_args()
 directory = args.directory
 
-if directory[-1] == "/":
-    directory = directory[:-1]
-
-file = directory.replace("/", "_")
-output_filename = file + ".tsv"
-
-print(output_filename)
+print(__generate_dataset_uuid(directory))

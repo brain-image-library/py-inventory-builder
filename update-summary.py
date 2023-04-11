72733dfbd6c9c02d8d80ed39db01d9e3c8f3f448
icaoberg <icaoberg@andrew.cmu.edu>
2023-04-11 16:34:27 -0400

    Adds  dependency, which increases speed. Adds two private functions that deal with getting the files in a directory and the number of files in a directory, respectively. The script now outputs two summary files:  and , one for existing files and another for non-existing ones.

--- update-summary.py
+++ update-summary.py
@@ -7,6 +7,7 @@ from pathlib import Path
 import tabulate
 import uuid
 import requests
+from pandarallel import pandarallel
 
 
 def pprint(msg):
@@ -20,6 +21,19 @@ def __update_dataframe(dataset, temp, key):
     for index, datum in temp.iterrows():
         dataset.loc[index, key] = temp.loc[index, key]
     return dataset
+
+
+def __get_files(directory):
+    files = subprocess.check_output(["lfs", "find", "-type", "f", directory])
+    files = str(files)
+    files = files.replace("b'/bil/data/", "/bil/data/")
+    files = files.split("\\n")
+    files = files[:-1]
+    return files
+
+
+def __get_number_of_files(directory):
+    return len(files)
 
 
 def generate_dataset_uuid(directory):
@@ -40,14 +54,41 @@ temp_file = Path(f"/tmp/summarymetadata.csv")
 if temp_file.exists():
     temp_file.unlink()
 
+# ncores=5
+# pandarallel.initialize(progress_bar=True, nb_workers=ncores)
+
 response = requests.get(url)
 temp_file.write_bytes(response.content)
 
 df = pd.read_csv(temp_file, sep=",")
 df["dataset_uuid"] = df["bildirectory"].apply(generate_dataset_uuid)
 df["exists"] = df["bildirectory"].apply(exists)
-df = df[df["exists"] == True]
 
+df2 = df[df["exists"] == False]
+df2 = df2[
+    [
+        "metadata_version",
+        "dataset_uuid",
+        "bildirectory",
+        "exists",
+        "project",
+        "is_biccn",
+        "bil_uuid",
+        "contributorname",
+        "affiliation",
+        "award_number",
+        "species",
+        "ncbitaxonomy",
+        "samplelocalid",
+        "genotype",
+        "generalmodality",
+        "technique",
+        "locations",
+        "URL",
+    ]
+]
+
+df = df[df["exists"] == True]
 df = df[
     [
         "metadata_version",
@@ -70,4 +111,6 @@ df = df[
         "URL",
     ]
 ]
+
 df.to_csv("summarymetadata.tsv", sep="\t", index=False)
+df2.to_csv("brokensummarymetadata.tsv", sep="\t", index=False)

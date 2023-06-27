109d48413523ddea4d6572066b519abeca54ba31
Ivan Cao-Berg <icaoberg@psc.edu>
2023-06-27 17:51:30 -0400

    Add conda environment activation to update-summary.sh for running as a cron job.

--- update-summary.sh
+++ update-summary.sh
@@ -4,4 +4,8 @@
 #SBATCH -N 1
 #SBATCH --mem=512G
 
+# this is only mean to aid @icaoberg run this as a cronjob
+
+. /bil/users/icaoberg/miniconda3/etc/profile.d/conda.sh
+conda activate base
 python ./update-summary.py
